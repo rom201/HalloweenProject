6b2fc94f798f114f2e4b1f6a2c5420896296f239
rom201 <kovalchukr1104@gmail.com>
2020-10-31 14:39:43 -0400

    add actions in branch action 3

--- src/test/java/FearureAcode.java
+++ src/test/java/FearureAcode.java
@@ -2,5 +2,6 @@ public class FearureAcode {
     public static void main(String[] args) {
         System.out.println("Work in branch1");
         System.out.println("Work in branch1 action 2");
+        System.out.println("Work in branch1 action 3");
     }
 }
