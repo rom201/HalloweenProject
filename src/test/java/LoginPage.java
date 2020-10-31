cb7cb99b4a3f1522d8ae6f553c57c82b6aaf937f
rom201 <kovalchukr1104@gmail.com>
2020-10-31 16:02:46 -0400

    actions login page & search page

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,5 @@
+public class LoginPage {
+    public static void main(String[] args) {
+        System.out.println("login page");
+    }
+}
---
+++ src/test/java/SearchPage.java
@@ -0,0 +1,5 @@
+public class SearchPage {
+    public static void main(String[] args) {
+        System.out.println("search");
+    }
+}
