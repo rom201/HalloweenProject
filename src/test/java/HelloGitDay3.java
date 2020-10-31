88a5780c141be6d0e5f26979d3c5baf3c229f171
rom201 <kovalchukr1104@gmail.com>
2020-10-31 12:45:58 -0400

    add action 3

--- src/test/java/HelloGitDay3.java
+++ src/test/java/HelloGitDay3.java
@@ -2,5 +2,6 @@ public class HelloGitDay3 {
     public static void main(String[] args) {
         System.out.println("Day3 action1");
         System.out.println("Day3 action2");
+        System.out.println("Day3 action3");
     }
 }
